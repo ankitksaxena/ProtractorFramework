7de8f763066bbe728b04f3e5926a1fa3d854702e
Saxena <ankit-saxena@MERCER.COM>
2020-09-07 15:42:25 +0530

    Commit Successful

---
+++ pages/homepage.js
@@ -0,0 +1,30 @@
+    let homepage = function(){
+        var firstNumber_input = element(by.model('first'));
+        var secondNumber_input = element(by.model('second'));
+        var goButton_input = element(by.css('[ng-click="doAddition()"]')); 
+
+
+        this.get = function(url){
+            browser.get(url);
+        };
+                    
+        this.enterFirstNumber = function(firstNo){
+            firstNumber_input.sendKeys(firstNo); 
+        };
+
+        this.enterSecondNumber = function(secondNo){
+           secondNumber_input.sendKeys(secondNo); 
+        };
+
+        this.clickGo = function(){
+            goButton_input.click();
+        };
+
+        this.verifyResult = function(result){
+            var output = element(by.cssContainingText('.ng-binding', result));
+             expect(output.getText()).toEqual(result);
+        };
+
+    };
+
+    module.exports = new homepage(); 
